8f4e083dddd7dc7cef7234e3717b0c63dde6495f
SiYoung <rollrang@gmail.com>
2025-12-08 00:06:51 +0900

    Add homepage for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Post from "./components/Post.js";
-import { posts } from "./data/post.js";
+import { posts } from "./data/posts.js";
 
 function App() {
   return (
